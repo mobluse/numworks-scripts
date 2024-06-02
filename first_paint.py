6d74646e1bfaa7f84b3e0621cba70263d3af188c
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-02 15:22:09 +0200

    Update first_paint.py

--- first_paint.py
+++ first_paint.py
@@ -7,13 +7,13 @@ def first_paint():
   y=100
   while 1:
     if keydown(KEY_LEFT):
-      x=x-1
+      x-=1
     if keydown(KEY_UP):
-      y=y-1
+      y-=1
     if keydown(KEY_RIGHT):
-      x=x+1
+      x+=1
     if keydown(KEY_DOWN):
-      y=y+1
+      y+=1
     set_pixel(x,y,color(0,255,0))
     time.sleep(0.1)
 first_paint()
