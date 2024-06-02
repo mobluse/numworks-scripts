1913b54f3afe188bdc80928147e53be047811cc3
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-02 15:36:37 +0200

    Update multable2.py

--- multable2.py
+++ multable2.py
@@ -1,10 +1,15 @@
 # Multiplication table
-print('* ', end='')
-for j in range(1, 10):
+u=10
+print(' * ', end='')
+for j in range(1, 9):
   print(end='%2d '%j)
+for j in range(9, u):
+  print(end='%3d '%j)
 print()
-for i in range(1, 10):
-  print(end='%1d:'%i)
-  for j in range(1, 10):
+for i in range(1, u):
+  print(end='%2d:'%i)
+  for j in range(1, 9):
     print(end='%2d ' % (i*j))
+  for j in range(9, u):
+    print(end='%3d ' % (i*j))
   print()
